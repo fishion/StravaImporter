11411e92368161c313f7465f68f23406b9a7ef83
Alex <alex@monney.me>
2020-11-17 21:30:16 +0000

    starting project

---
+++ index.js
@@ -0,0 +1,18 @@
+// Constants
+
+const REQUEST_BUFFER_SECONDS = 40
+const MAX_REQUEST_DAY = 1000
+
+console.log('hi')
+
+// print a link to Strava OAuth page
+
+// User needs to go to link, authorise and submit
+
+// User redirected to page including authorisation code and scope - how long is this valid for?
+
+// User pastes the url into script input - verify scope and then request short lived access token
+
+// Receive Access Token, Access token expiration date, refresh token
+
+
