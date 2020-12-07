0f7ee81ee4f4482a5f5ec631b34ebdfee921082f
Alex <alex@monney.me>
2020-12-07 22:36:19 +0000

    bigish refactor

--- index.js
+++ index.js
@@ -2,20 +2,11 @@
 
 'use strict;'
 
-const files = require('./lib/files')
 const strava = require('./lib/strava');
 
 (async () => {
-
-  // check data directories
-  await files.verifyData()
-
-  // set up strava client config
-  await strava.authorise();
-
   // upload the tracks
   await strava.upload()
 
   process.exit(0);
-
 })();
